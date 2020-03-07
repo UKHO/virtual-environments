c6cbd78150fd442e31875c47cbcec6ae499b93e9
ElDuddz <elduddz@outlook.com>
2020-03-07 00:12:44 +0000

    Improve process and logic around check

--- images/win/scripts/Installers/Install-PowershellCore.ps1
+++ images/win/scripts/Installers/Install-PowershellCore.ps1
@@ -2,22 +2,4 @@
 ##  File:  Install-PowershellCore.ps1
 ##  Desc:  Install PowerShell Core
 ################################################################################
-$condition = $false
-$count = 0
-$install = "powershell-core"
-do {
-    $installed = powershell choco list -lo $install --all
-    if ($installed -notcontains "$install") {
-        Write-Output "running: powershell choco install $install -y"
-        powershell choco install $install -y
-    }
-    else {
-        Write-Output "package found: $install"
-        $condition = $true
-    }
-    $count++
-    if($count -eq 5) {
-        Write-Error "Could not install $install after $count attempts"
-        exit 1
-    }
-} while ($condition -eq $false)
+Install-Choco "powershell-core"
