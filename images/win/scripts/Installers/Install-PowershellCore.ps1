c66628ef20fd625a7b55a89578f5dd5fde7d6c29
ElDuddz <elduddz@outlook.com>
2020-03-09 23:24:11 +0000

    Use Install-Choco

--- images/win/scripts/Installers/Install-PowershellCore.ps1
+++ images/win/scripts/Installers/Install-PowershellCore.ps1
@@ -2,5 +2,4 @@
 ##  File:  Install-PowershellCore.ps1
 ##  Desc:  Install PowerShell Core
 ################################################################################
-
-Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
+Install-Choco "powershell-core"
